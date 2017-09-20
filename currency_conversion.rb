4aefa728bc33fd6e456e0ce5eb7ea35ca1224de3
Glenn Dimaliwat <glenn.dimaliwat@gmail.com>
2017-09-20 22:39:25 +1000

    Use hashes for currency conversion

--- currency_conversion.rb
+++ currency_conversion.rb
@@ -1,53 +1,59 @@
 #You took a job in Shanghai along with Pat working for a financial services company. Your boss is Chinese (obviously), a bit of a madman, and dislikes the way the West has 'taken over'. He wants you to write a currency exchange method, but each currency must only be compared to the Chinese renminbi. Write some methods, each that take a number representing a currency (let's say Thai baht, or Indonesian rupiah, and whatever you like), and converts each into the number of Chinese renminbi. Now write a method that takes a number, and two strings. Using only the methods you have created, code this last method so that you can specify 'from' and 'to' currencies (string arguments), and a number, and have this return the relevant exchange. For example, you could accept 'vnd' (the Viet dong), 'hkd' (Honky dollar), and a number, and gives the correct exchange for that number given those country parameters. Please store I a w-n variable!
 
-def convert_currency(money, exchange_rate)
-    money * exchange_rate
+def convert_currency(money, exchange_rate, reversed)
+    if reversed == false
+        money * exchange_rate
+    else
+        money / exchange_rate
+    end
 end
 
-def exchange_money(from_currency, to_currency, money)
-
-    exchange_rate_data = [
-        {   # 1 Thai Baht = 0.20 Chinese Renminbi
-            from_currency: "thb",
-            to_currency: "cny",
-            exchange_rate: 0.20
-        },
-        {   # 1 Viet Dong = 0.00034 Hongkong Dollar
-            from_currency: "vnd",
-            to_currency: "hkd",
-            exchange_rate: 0.00034
-        }
-    ]
+def exchange_money(exchange_rate_data, from_currency, to_currency, money)
 
     exchange_rate = 0
+    result = 0
 
-    
-    # if from_currency=="thb" and to_currency=="cny" # 1 Thai Baht = 0.20 Chinese Renminbi
-    #     exchange_rate = 0.20
-
-    # elsif from_currency=="vnd" and to_currency=="hkd" # 1 Viet Dong = 0.00034 Hongkong Dollar
-    #     exchange_rate = 0.00034
-
-    # end
-
+    # Loop each exchange rate data
     exchange_rate_data.each { |exchange|
-        if from_currency==exchange[:from_currency] and exchange[:to_currency]
+
+        # Check if from_currency and to_currency matches a record in exchange rate data
+        if from_currency==exchange[:from_currency] and to_currency==exchange[:to_currency]  # Normal
             exchange_rate = exchange[:exchange_rate]
+            result = convert_currency(money, exchange_rate, false)
+            break
+        elsif from_currency==exchange[:to_currency] and to_currency==exchange[:from_currency]   # Reversed
+            exchange_rate = exchange[:exchange_rate]
+            result = convert_currency(money, exchange_rate, true)
             break
         end
     }
-
-    convert_currency(money, exchange_rate)
+    result
 end
 
+exchange_rate_data = [
+    {   # 1 Thai Baht = 0.20 Chinese Renminbi
+        from_currency: "thb",
+        to_currency: "cny",
+        exchange_rate: 0.20
+    },
+    {   # 1 Viet Dong = 0.00034 Hongkong Dollar
+        from_currency: "vnd",
+        to_currency: "hkd",
+        exchange_rate: 0.00034
+    }
+]
 
+# Should output 20.0
+p exchange_money(exchange_rate_data, "thb","cny", 100)
 
+# Should output 100.0
+p exchange_money(exchange_rate_data, "cny","thb", 20)
 
+# Should output 0.034
+p exchange_money(exchange_rate_data, "vnd","hkd", 100)
 
-p exchange_money("thb","cny", 100)
-p exchange_money("vnd","hkd", 100)
-p exchange_money("aud","php", 100)
+# Should output 100.0
+p exchange_money(exchange_rate_data, "hkd","vnd", 0.034)
 
-
-# p one_hundred_thb_to_cny
-# p one_hundred_vnd_to_hkd
+# Should output 0 since AUD to PHP is not on the list
+p exchange_money(exchange_rate_data, "aud","php", 500)
